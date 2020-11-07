2948d8f3e64ca24c204ebe6af295d7aa10153b03
JasPradhan <70465360+JasPradhan@users.noreply.github.com>
2020-11-07 13:01:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+objects=[];
+
 video="";
 
 status="";
@@ -24,7 +26,31 @@ canvas.center()
 
 function draw(){
 image(video,0,0,480,380);
-video.loop();
-video.volume(0);
-video.speed(2.5);
+video.loop(); 
+video.volume(1);
+video.speed(1);
+if(status!=""){
+    objectDetector.detect(video,gotResults);
+    for(i=0;i<objects.length;i++){
+        document.getElementById("number_of_objects").innerHTML="Number Of Objects Detected Are: "+objects.length;
+
+        document.getElementById("status").innerHTML="Objects Detected";
+        fill("#09ff00");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percent+"%",objects[i].x+10,objects[i].y+10);
+        noFill();
+        stroke("#09ff00");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+}
+}
+
+function gotResults(error,results){
+        if(error){
+            console.error(error);
+        }
+        else{
+            console.log(results);
+            objects=results;
+        }
 }
